538cc66f4a596a57352cb1138aec2ed88bef38e7
Nikhil Gupta <nikhilgupta@192.168.1.102>
2025-01-15 20:39:31 +0530

    exploring the get,post ,delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ const express = require("express")
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("sended some data")
+
+app.get("/user",(req,res)=>{
+    res.send({username:"someUser",pass:"pass key"})
 })
+
+
+app.post("/user",(req,res)=>{
+    res.send("Data added successfully")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data Deleted successfully")
+})
+
+
+app.get("/test",(req,res)=>{
+    res.send("calling for test")
+})
+
+
 
 app.listen(3000,()=>{
     console.log("server is runing on ",3000)
