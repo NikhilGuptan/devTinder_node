0ef7fc4681b5a620e4c72125c517ebab9c4672ad
Nikhil Gupta <nikhilgupta@192.168.1.16>
2025-01-17 19:45:00 +0530

    added feed api and find usewr by email

--- src/app.js
+++ src/app.js
@@ -9,15 +9,39 @@ const User = require("./models/user");
 
 app.post("/signup",async (req,res)=>{
     const data = req.body;
-
     const user = new User(data);
-
     try{
         await user.save();
         res.send("user added succsfully")
     }
     catch(err){
         res.status(400).send("Some error while saving user")
+    }
+})
+
+
+app.post("/users",async (req,res)=>{
+    const email = req.body.emailId;
+
+    try{
+       const usersData =  await User.find({emailId:email});
+       res.send(usersData);
+
+    }catch(err){
+        res.status(400).send("Something went wroung")
+    }
+})
+
+
+app.get("/feed",async (req,res)=>{
+    const email = req.body.emailId;
+
+    try{
+       const usersData =  await User.find({});
+       res.send(usersData);
+
+    }catch(err){
+        res.status(400).send("Something went wroung")
     }
 })
 
