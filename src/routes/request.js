c2bfc4d3d77f16bc29791cf03fe85716c74aa61d
Nikhil Gupta <nikhilgupta@Nikhils-MacBook-Air.local>
2025-01-25 17:57:53 +0530

    added request send api

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,19 +1,69 @@
 const express = require("express");
-const  {userAuth} = require("../middleWare/auth");
-
 const requestRouter = express.Router();
 
+const { userAuth } = require("../middleWare/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.get("/sendConcetionRequest", userAuth ,async (req, res) => {
+// const sendEmail = require("../utils/sendEmail");
+
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
     try {
-  
-      const user = req.user;
-      res.send(`concetion Request send from : ${user.firstName}`);
-    } catch (err) {
-      res.status(500).send("Something went wrong: " + err.message);
-    }
-  });
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists!!" });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+    //   const emailRes = await sendEmail.run(
+    //     "A new friend request from " + req.user.firstName,
+    //     req.user.firstName + " is " + status + " in " + toUser.firstName
+    //   );
+    //   console.log(emailRes);
+
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  }
+);
 
 
 module.exports = requestRouter;
